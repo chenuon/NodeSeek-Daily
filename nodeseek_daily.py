704a5839831ba4a05fc0f42dda95da1bcba68fce
chenuon <59986158+chenuon@users.noreply.github.com>
2025-04-02 11:32:41 +0800

    Update nodeseek_daily.py

--- nodeseek_daily.py
+++ nodeseek_daily.py
@@ -22,7 +22,7 @@ cookie = os.environ.get("NS_COOKIE") or os.environ.get("COOKIE")
 # 通过环境变量控制是否使用无头模式，默认为 True（无头模式）
 headless = os.environ.get("HEADLESS", "true").lower() == "true"
 
-randomInputStr = ["bd", "绑定", "帮顶"]
+randomInputStr = ["bd", "绑定", "帮顶", "帮群主D一下", "羡慕"]
 
 # 获取系统中 Chrome 的版本
 def get_chrome_version():
